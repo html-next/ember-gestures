c52c52f74ce638b650ed4c81006d8b5fc45a9a1b
Chris Thoburn <runspired@gmail.com>
2015-08-18 01:28:11 -0500

    bump hammer-time dependency (will need bumped again tomorrow to match bugfix release for bubble capture

--- blueprints/ember-gestures/index.js
+++ blueprints/ember-gestures/index.js
@@ -7,7 +7,7 @@ module.exports = {
   afterInstall: function() {
     var bowerPackages = [
       { name: 'runspired/hammer.js', target: 'develop' },
-      { name: 'hammer-time', target: '0.2.0'}
+      { name: 'hammer-time', target: '0.2.2'}
     ];
     return this.addBowerPackagesToProject(bowerPackages);
   }
