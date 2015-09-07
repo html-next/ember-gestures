8f1db2d2cc3505eb2c7139b12e0ef784b1131473
Tom Spacek <tomspacek@gmail.com>
2015-09-08 09:32:56 +1000

    Switches to latest version of hammer-time

--- blueprints/ember-gestures/index.js
+++ blueprints/ember-gestures/index.js
@@ -7,7 +7,7 @@ module.exports = {
   afterInstall: function() {
     var bowerPackages = [
       { name: 'hammer.js', source: 'git@github.com:runspired/hammer.js.git', target: 'develop' },
-      { name: 'hammer-time', target: '0.2.2'}
+      { name: 'hammer-time', source: 'git@github.com:hammerjs/hammer-time.git', target: 'master' }
     ];
     return this.addBowerPackagesToProject(bowerPackages);
   }
