6e39c4b6e84b1c70aa7411e7de74b4dd5325da52
Erik Trom <erik.trom@gmail.com>
2015-12-08 10:35:00 -0800

    setup ember-try w/ travis.ci

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,8 +1,14 @@
+/*jshint node:true*/
 module.exports = {
   scenarios: [
     {
       name: 'default',
-      dependencies: { }
+      dependencies: {
+        'ember': '2.2.0'
+      },
+      resolutions: {
+        'ember': '2.2.0'
+      }
     },
     {
       name: 'ember-release',
@@ -29,6 +35,24 @@ module.exports = {
       },
       resolutions: {
         'ember': 'canary'
+      }
+    },
+    {
+      name: '1.10',
+      dependencies: {
+        'ember': '~1.10.0'
+      }
+    },
+    {
+      name: '1.11',
+      dependencies: {
+        'ember': '~1.11.0'
+      }
+    },
+    {
+      name: '1.12',
+      dependencies: {
+        'ember': '~1.12.0'
       }
     }
   ]
