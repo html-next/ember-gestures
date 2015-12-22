18756a17808c18100db06799da93f6860714f8aa
Chris Thoburn <runspired@gmail.com>
2015-12-22 15:32:33 -0600

    updates dependencies

--- blueprints/ember-gestures/index.js
+++ blueprints/ember-gestures/index.js
@@ -6,7 +6,7 @@ module.exports = {
 
   afterInstall: function() {
     var bowerPackages = [
-      //{ name: 'hammer.js', target: '2.1.0' },
+      { name: 'hammer.js', target: '2.0.5' },
       { name: 'hammer-time', target: '0.3.0'}
     ];
     return this.addBowerPackagesToProject(bowerPackages);
