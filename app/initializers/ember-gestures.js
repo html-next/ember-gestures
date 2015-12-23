c16bfd29ae4dd14b09b6e17c284973e37edbb065
Chris Thoburn <runspired@gmail.com>
2015-12-23 12:25:35 -0600

    cleans up initializers

--- app/initializers/ember-gestures.js
+++ app/initializers/ember-gestures.js
@@ -1,12 +1,11 @@
 // activate touch action css
-import LinkComponent from 'ember-gestures/components/link-component';
+import LinkComponent from 'ember-gestures/components/link-component'; // jshint ignore:line
+import Component from 'ember-gestures/components/component'; // jshint ignore:line
 
 export default {
 
   name: 'ember-gestures',
 
-  initialize: function() {
+  initialize: function() {}
 
-  }
-
-}
+};
--- app/instance-initializers/ember-gestures.js
+++ app/instance-initializers/ember-gestures.js
@@ -5,4 +5,4 @@ export default {
     instance.container.lookup('service:-gestures');
   }
 
-}
+};
