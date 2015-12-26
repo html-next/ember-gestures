d755738f0e8ebc44282915de68066b787eefd7e9
Kevin Bullaughey <kbullaughey@gmail.com>
2015-12-26 15:05:40 -0500

    fix(build): with only one hammerjs location, path needed updating

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ module.exports = {
   included: function (app) {
     app.import('vendor/ember-gestures/dom-guard-begin.js');
     if (app.env === "production") {
-      app.import(app.bowerDirectory + '/hammerjs/hammer.min.js');
+      app.import(app.bowerDirectory + '/hammer.js/hammer.min.js');
     } else {
-      app.import(app.bowerDirectory + '/hammerjs/hammer.js');
+      app.import(app.bowerDirectory + '/hammer.js/hammer.js');
     }
     app.import(app.bowerDirectory + '/hammer-time/hammer-time.js');
     app.import('vendor/ember-gestures/dom-guard-end.js');
