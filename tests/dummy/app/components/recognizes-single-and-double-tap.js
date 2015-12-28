7cc3ad76b21125a647177f6460d59ff6acb27d0b
Chris Thoburn <runspired@gmail.com>
2015-12-28 15:19:13 -0600

    cleanup module es6 usage

--- tests/dummy/app/components/recognizes-single-and-double-tap.js
+++ tests/dummy/app/components/recognizes-single-and-double-tap.js
@@ -2,20 +2,25 @@ import Ember from 'ember';
 import layout from '../templates/components/recognizes-single-and-double-tap';
 import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 
-export default Ember.Component.extend(RecognizerMixin, {
-  layout: layout,
+const {
+  Component,
+  run
+  } = Ember;
+
+export default Component.extend(RecognizerMixin, {
+  layout,
   recognizers: 'tap double-tap',
   sawSingle: false,
   sawDouble: false,
 
   tap() {
     this.set('sawSingle', true);
-    Ember.run.later, this, this.reset, 1000
+    run.later(this, this.reset, 1000);
   },
 
   doubleTap() {
     this.set('sawDouble', true);
-    Ember.run.later, this, this.reset, 1000
+    run.later(this, this.reset, 1000);
   },
 
   reset() {
