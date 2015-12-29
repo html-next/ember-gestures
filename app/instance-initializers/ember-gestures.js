755f19645c5b66b6d2cb40f98333a26eb468ed3b
davidgoli <davigoli@gmail.com>
2015-12-29 16:24:00 -0600

    fix initializer warning

--- app/instance-initializers/ember-gestures.js
+++ app/instance-initializers/ember-gestures.js
@@ -2,7 +2,7 @@ export default {
   name: 'ember-gestures',
 
   initialize: function (instance) {
-    instance.container.lookup('service:-gestures');
+    instance.lookup('service:-gestures');
   }
 
 };
