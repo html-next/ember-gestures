bfba971b79db083e8ca3f76366f4934713dd41b9
Frederik Bosch <f.bosch@genkgo.nl>
2016-10-11 19:27:17 +0200

    Make an escape of fast focus

--- addon/event_dispatcher.js
+++ addon/event_dispatcher.js
@@ -84,7 +84,7 @@ export default EventDispatcher.extend({
         if ($element.is('textarea') || ($element.is('input') && notFocusableTypes.indexOf($element.attr('type')) === -1) && !$element.hasClass('no-fast-focus')) {
           $element.focus();
 
-        } else if ($target.is('textarea') || ($target.is('input') && notFocusableTypes.indexOf($target.attr('type')) === -1) && !$element.hasClass('no-fast-focus')) {
+        } else if ($target.is('textarea') || ($target.is('input') && notFocusableTypes.indexOf($target.attr('type')) === -1) && !$target.hasClass('no-fast-focus')) {
           $target.focus();
         }
       }
