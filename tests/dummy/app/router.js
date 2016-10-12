0fe4b22d300b6fd690862d7d82693dd6a6eabd0b
paul <abrahamspaa@gmail.com>
2016-10-12 14:20:23 +0100

    Ember upgrade to 2.8.0

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-  location: config.locationType
+const { Router } = Ember;
+
+const appRouter = Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
-Router.map(function() {
+appRouter.map(function() {
   this.route('taps');
 });
 
-export default Router;
+export default appRouter;
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,15 +2,14 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
-const merge = Ember.assign || Ember.merge;
-
+const { merge, run } = Ember;
 export default function startApp(attrs) {
   let application;
 
   let attributes = merge({}, config.APP);
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
-  Ember.run(() => {
+  run(() => {
     application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
